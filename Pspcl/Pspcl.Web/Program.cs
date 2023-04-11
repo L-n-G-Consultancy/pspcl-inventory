9513eeeb610ff0fabcccf19ebf2366abe467b581
simranLnG <simran@lng-consultancy.com>
2023-04-11 14:49:51 +0530

    Serilog implemented

--- Pspcl/Pspcl.Web/Program.cs
+++ Pspcl/Pspcl.Web/Program.cs
@@ -1,27 +1,63 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+using Serilog;
+//using Serilog.Sinks.RollingFile;
+using Serilog.Sinks.File;
+using Microsoft.AspNetCore.Builder.Extensions;
 
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace Pspcl.Web
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+            var builder = WebApplication.CreateBuilder(args);
+
+
+
+
+            var _logger = new LoggerConfiguration()
+            .MinimumLevel.Information().WriteTo
+                         .Map("UtcDateTime", DateTime.UtcNow.ToString("ddMMyyyy"), (UtcDateTime, wt) => wt.File($"Logs/Log_{UtcDateTime}.txt"))
+                         .CreateLogger();
+
+            builder.Logging.AddSerilog(_logger);
+
+
+
+            builder.Services.AddControllersWithViews();
+
+
+            var app = builder.Build();
+
+
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+            app.Run();
+
+
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
