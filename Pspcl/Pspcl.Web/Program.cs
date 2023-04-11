50057eaeab16ced0cb8f09ec49fa67bd395a4f9b
sachreet <sachreet.kaur@lng-consultancy.com>
2023-04-11 16:48:08 +0530

    removed custom controller

--- Pspcl/Pspcl.Web/Program.cs
+++ Pspcl/Pspcl.Web/Program.cs
@@ -29,7 +29,8 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    //name: "default",
+    //pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
 
 app.Run();
