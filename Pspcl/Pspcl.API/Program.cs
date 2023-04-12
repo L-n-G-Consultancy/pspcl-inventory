b25de51533160994c5accefc7bad44cece367c4d
abhijeetkumarLnG <abhijeet.kumar@lng-consultancy.com>
2023-04-12 17:52:46 +0530

    Added Swagger and home Controller

--- Pspcl/Pspcl.API/Program.cs
+++ Pspcl/Pspcl.API/Program.cs
@@ -1,17 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Builder;
 
-// Add services to the container.
 
-builder.Services.AddControllers();
 
-var app = builder.Build();
+namespace Pspcl.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-// Configure the HTTP request pipeline.
+            // Add services to the container.
+           
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
-app.UseHttpsRedirection();
+            var app = builder.Build();
 
-app.UseAuthorization();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
-app.MapControllers();
+            app.UseHttpsRedirection();
 
-app.Run();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
--- Pspcl/Pspcl.Web/Program.cs
+++ Pspcl/Pspcl.Web/Program.cs
@@ -23,8 +23,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-   // name: "default",
-   // pattern: "{controller=Home}/{action=Index}/{id?}"
-    );
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
