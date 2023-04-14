6e557526beed0a987c641f3c2b4c12d381c022a4
princekumarLnG <prince.kumar@lng-consultancy.com>
2023-04-14 16:14:56 +0530

    created login page UI & set it Default page

---
+++ Pspcl/Pspcl.Web/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace Pspcl.Web.Controllers
+{
+   
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+    }
+}
