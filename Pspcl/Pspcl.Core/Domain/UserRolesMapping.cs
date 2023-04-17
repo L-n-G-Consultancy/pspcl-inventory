d4046a98aaf54cb9dc1860529c4b947ab30abd99
sachreet <sachreet.kaur@lng-consultancy.com>
2023-04-17 16:37:24 +0530

    Added Role entity in domain

--- Pspcl/Pspcl.Core/Domain/UserRolesMapping.cs
+++ Pspcl/Pspcl.Core/Domain/UserRolesMapping.cs
@@ -11,7 +11,6 @@ namespace Pspcl.Core.Domain
 {
     public class UserRolesMapping
     {
-        public int Id { get; set; } // Primary key
         public int UserId { get; set; } // Foreign key to User
         public int RoleId { get; set; } // Foreign key to Roles
         public User User { get; set; }
